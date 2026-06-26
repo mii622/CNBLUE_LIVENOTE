40ace542eeca8ef89893d1e377fa008ef83a364f
mii622 <nonnon6219@gmail.com>
2026-06-27 02:06:38 +0900

    Update trc.js

--- Test/data/trc.js
+++ Test/data/trc.js
@@ -9,14 +9,14 @@ const CONFIG = {
     { left: "37.5%", top: "16%", type: "top" },
     { left: "47.5%", top: "16%", type: "top" },
 
-    { left: "56.5%", top: "16%", type: "top" },
-    { left: "63.5%", top: "16%", type: "top" },
+    { left: "55.5%", top: "16%", type: "top" },
+    { left: "60.5%", top: "16%", type: "top" },
     { left: "76.5%", top: "16%", type: "top" },
-    { left: "80.5%", top: "16%", type: "top" },
-    { left: "85.5%", top: "16%", type: "top" },
+    { left: "82.5%", top: "16%", type: "top" },
+    { left: "88.5%", top: "16%", type: "top" },
  
 
-    { left: "12.5%", top: "54%", type: "bottom" },
+    { left: "11.5%", top: "54%", type: "bottom" },
     { left: "20.5%", top: "54%", type: "bottom" },
     { left: "28.5%", top: "54%", type: "bottom" },
     { left: "37.5%", top: "54%", type: "bottom" },
