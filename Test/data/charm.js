7f21cfb98504047b6255f9cd0ca01ea6638a6a95
mii622 <nonnon6219@gmail.com>
2026-06-27 02:45:53 +0900

    Update charm.js

--- Test/data/charm.js
+++ Test/data/charm.js
@@ -9,20 +9,20 @@ const CONFIG = {
     { left: "73.5%", top: "16%", type: "top" },
     { left: "88.5%", top: "16%", type: "top" },
 
-    { left: "4.5%", top: "47%", type: "top" },
+    { left: "3.5%", top: "45%", type: "top" },
     { left: "14.5%", top: "50%", type: "top" },
     { left: "27.5%", top: "50%", type: "top" },
     { left: "37.5%", top: "50%", type: "top" },
     { left: "50.5%", top: "50%", type: "top" },
-    { left: "63.5%", top: "47%", type: "top" },
+    { left: "63.5%", top: "40%", type: "top" },
     { left: "73.5%", top: "50%", type: "top" },
     { left: "85.5%", top: "50%", type: "top" },
     
-    { left: "15.5%", top: "83%", type: "top" },
-    { left: "25.5%", top: "83%", type: "top" },
-    { left: "37.5%", top: "81%", type: "top" },
-    { left: "50.5%", top: "81%", type: "top" },
-    { left: "62.5%", top: "83%", type: "top" },
-    { left: "73.5%", top: "83%", type: "top" }
+    { left: "15.5%", top: "75%", type: "top" },
+    { left: "25.5%", top: "75%", type: "top" },
+    { left: "37.5%", top: "75%", type: "top" },
+    { left: "50.5%", top: "75%", type: "top" },
+    { left: "62.5%", top: "75%", type: "top" },
+    { left: "73.5%", top: "75%", type: "top" }
   ]
 };
