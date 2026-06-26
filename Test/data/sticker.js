5a7b9c511061cd4cd9a3f1c5b2f253c589704cfa
mii622 <nonnon6219@gmail.com>
2026-06-27 03:10:47 +0900

    Update sticker.js

--- Test/data/sticker.js
+++ Test/data/sticker.js
@@ -6,9 +6,9 @@ const CONFIG = {
 
   cards: [
     { left: "2%", top: "16%", type: "top" },
-    { left: "24%", top: "20%", type: "top" },
+    { left: "24%", top: "32%", type: "top" },
     { left: "43%", top: "16%", type: "top" },
-    { left: "62%", top: "20%", type: "top" },
+    { left: "62%", top: "32%", type: "top" },
     { left: "81%", top: "16%", type: "top" },
   ]
 };
