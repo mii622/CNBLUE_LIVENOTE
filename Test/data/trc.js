a112c78124526a2d25205e5132d6b925601f1e12
mii622 <nonnon6219@gmail.com>
2026-06-27 01:53:43 +0900

    Update trc.js

--- Test/data/trc.js
+++ Test/data/trc.js
@@ -2,27 +2,28 @@ const CONFIG = {
   image: "../img/26bktrc.jpg",
 
   cards: [
-    { left: "7.5%", top: "16%", type: "top" },
-    { left: "15.5%", top: "16%", type: "top" },
-    { left: "23.5%", top: "16%", type: "top" },
-    { left: "31.5%", top: "16%", type: "top" },
-    { left: "39.5%", top: "16%", type: "top" },
+    { left: "1.5%", top: "16%", type: "top" },
+    { left: "9.5%", top: "16%", type: "top" },
+    { left: "17.5%", top: "16%", type: "top" },
+    { left: "25.5%", top: "16%", type: "top" },
+    { left: "33.5%", top: "16%", type: "top" },
+    { left: "41.5%", top: "16%", type: "top" },
 
     { left: "47.5%", top: "16%", type: "top" },
     { left: "55.5%", top: "16%", type: "top" },
     { left: "62.5%", top: "16%", type: "top" },
     { left: "70.5%", top: "16%", type: "top" },
     { left: "78.5%", top: "16%", type: "top" },
-    { left: "86.5%", top: "16%", type: "top" },
+ 
 
-    { left: "14.5%", top: "54%", type: "bottom" },
-    { left: "22.5%", top: "54%", type: "bottom" },
-    { left: "30.5%", top: "54%", type: "bottom" },
-    { left: "38.5%", top: "54%", type: "bottom" },
-    { left: "46.5%", top: "54%", type: "bottom" },
-    { left: "54.5%", top: "54%", type: "bottom" },
-    { left: "62.5%", top: "54%", type: "bottom" },
-    { left: "70.5%", top: "54%", type: "bottom" },
-    { left: "78.5%", top: "54%", type: "bottom" }
+    { left: "12.5%", top: "54%", type: "bottom" },
+    { left: "20.5%", top: "54%", type: "bottom" },
+    { left: "28.5%", top: "54%", type: "bottom" },
+    { left: "36.5%", top: "54%", type: "bottom" },
+    { left: "44.5%", top: "54%", type: "bottom" },
+    { left: "52.5%", top: "54%", type: "bottom" },
+    { left: "60.5%", top: "54%", type: "bottom" },
+    { left: "68.5%", top: "54%", type: "bottom" },
+    { left: "76.5%", top: "54%", type: "bottom" }
   ]
 };
