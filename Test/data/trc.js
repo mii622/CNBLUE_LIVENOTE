e01f03711a8c7c3159e6d37532c72b0211790278
mii622 <nonnon6219@gmail.com>
2026-06-27 02:00:31 +0900

    Update trc.js

--- Test/data/trc.js
+++ Test/data/trc.js
@@ -2,28 +2,28 @@ const CONFIG = {
   image: "../img/26bktrc.jpg",
 
   cards: [
-    { left: "1.5%", top: "16%", type: "top" },
-    { left: "9.5%", top: "16%", type: "top" },
-    { left: "17.5%", top: "16%", type: "top" },
-    { left: "25.5%", top: "16%", type: "top" },
-    { left: "33.5%", top: "16%", type: "top" },
-    { left: "41.5%", top: "16%", type: "top" },
-
+    { left: "2.5%", top: "16%", type: "top" },
+    { left: "11.5%", top: "16%", type: "top" },
+    { left: "18.5%", top: "16%", type: "top" },
+    { left: "28.5%", top: "16%", type: "top" },
+    { left: "37.5%", top: "16%", type: "top" },
     { left: "47.5%", top: "16%", type: "top" },
-    { left: "55.5%", top: "16%", type: "top" },
-    { left: "62.5%", top: "16%", type: "top" },
-    { left: "70.5%", top: "16%", type: "top" },
-    { left: "78.5%", top: "16%", type: "top" },
+
+    { left: "56.5%", top: "16%", type: "top" },
+    { left: "63.5%", top: "16%", type: "top" },
+    { left: "76.5%", top: "16%", type: "top" },
+    { left: "80.5%", top: "16%", type: "top" },
+    { left: "85.5%", top: "16%", type: "top" },
  
 
     { left: "12.5%", top: "54%", type: "bottom" },
     { left: "20.5%", top: "54%", type: "bottom" },
     { left: "28.5%", top: "54%", type: "bottom" },
-    { left: "36.5%", top: "54%", type: "bottom" },
-    { left: "44.5%", top: "54%", type: "bottom" },
-    { left: "52.5%", top: "54%", type: "bottom" },
-    { left: "60.5%", top: "54%", type: "bottom" },
-    { left: "68.5%", top: "54%", type: "bottom" },
-    { left: "76.5%", top: "54%", type: "bottom" }
+    { left: "37.5%", top: "54%", type: "bottom" },
+    { left: "45.5%", top: "54%", type: "bottom" },
+    { left: "54.5%", top: "54%", type: "bottom" },
+    { left: "63.5%", top: "54%", type: "bottom" },
+    { left: "71.5%", top: "54%", type: "bottom" },
+    { left: "80.5%", top: "54%", type: "bottom" }
   ]
 };
