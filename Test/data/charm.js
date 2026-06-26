556c40dea883adcd7369b1a69a2fd59f98146539
mii622 <nonnon6219@gmail.com>
2026-06-27 02:26:07 +0900

    Update charm.js

--- Test/data/charm.js
+++ Test/data/charm.js
@@ -2,27 +2,27 @@ const CONFIG = {
   image: "../img/26bkcharm.jpg",
 
   cards: [
-    { left: "3.5%", top: "16%", type: "top" },
-    { left: "11.5%", top: "16%", type: "top" },
-    { left: "20.5%", top: "16%", type: "top" },
     { left: "28.5%", top: "16%", type: "top" },
     { left: "37.5%", top: "16%", type: "top" },
-
-    { left: "46.5%", top: "16%", type: "top" },
-    { left: "55.5%", top: "16%", type: "top" },
+    { left: "50.5%", top: "16%", type: "top" },
     { left: "63.5%", top: "16%", type: "top" },
-    { left: "71.5%", top: "16%", type: "top" },
-    { left: "80.5%", top: "16%", type: "top" },
+    { left: "73.5%", top: "16%", type: "top" },
     { left: "88.5%", top: "16%", type: "top" },
 
-    { left: "13.5%", top: "57%", type: "bottom" },
-    { left: "21.5%", top: "57%", type: "bottom" },
-    { left: "29.5%", top: "57%", type: "bottom" },
-    { left: "38.5%", top: "57%", type: "bottom" },
-    { left: "46.5%", top: "57%", type: "bottom" },
-    { left: "55.5%", top: "57%", type: "bottom" },
-    { left: "63.5%", top: "57%", type: "bottom" },
-    { left: "72.5%", top: "57%", type: "bottom" },
-    { left: "79.5%", top: "57%", type: "bottom" }
+    { left: "11.5%", top: "60%", type: "top" },
+    { left: "18.5%", top: "53%", type: "top" },
+    { left: "28.5%", top: "53%", type: "top" },
+    { left: "37.5%", top: "53%", type: "top" },
+    { left: "50.5%", top: "53%", type: "top" },
+    { left: "63.5%", top: "53%", type: "top" },
+    { left: "73.5%", top: "53%", type: "top" },
+    { left: "86.5%", top: "53%", type: "top" },
+    
+    { left: "18.5%", top: "60%", type: "top" },
+    { left: "28.5%", top: "60%", type: "top" },
+    { left: "37.5%", top: "60%", type: "top" },
+    { left: "50.5%", top: "60%", type: "top" },
+    { left: "63.5%", top: "60%", type: "top" },
+    { left: "73.5%", top: "60%", type: "top" }
   ]
 };
