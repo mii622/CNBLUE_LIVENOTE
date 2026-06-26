eaf2a6b3e123f316f75fbccee8bb9484f3c00274
mii622 <nonnon6219@gmail.com>
2026-06-27 02:43:54 +0900

    Update and rename key.js to sticker.js

---
+++ Test/data/sticker.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  image: "../img/26bksticker.jpg",
+
+  cards: [
+    { left: "2.5%", top: "16%", type: "top" },
+    { left: "11.5%", top: "16%", type: "top" },
+    { left: "18.5%", top: "16%", type: "top" },
+    { left: "28.5%", top: "16%", type: "top" },
+    { left: "37.5%", top: "16%", type: "top" },
+  ]
+};
