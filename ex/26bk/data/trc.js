34100e4c60598cb841141fe779912159cdc24f6f
mii622 <nonnon6219@gmail.com>
2026-07-01 12:44:54 +0900

    Create trc.js

---
+++ ex/26bk/data/trc.js
@@ -0,0 +1,29 @@
+const CONFIG = {
+  image: "../img/26bktrc.jpg",
+
+  cards: [
+    { left: "2.5%", top: "16%", type: "top" },
+    { left: "11.5%", top: "16%", type: "top" },
+    { left: "18.5%", top: "16%", type: "top" },
+    { left: "28.5%", top: "16%", type: "top" },
+    { left: "37.5%", top: "16%", type: "top" },
+    { left: "47.5%", top: "16%", type: "top" },
+
+    { left: "55.5%", top: "16%", type: "top" },
+    { left: "65.5%", top: "16%", type: "top" },
+    { left: "74.5%", top: "16%", type: "top" },
+    { left: "83.5%", top: "16%", type: "top" },
+    { left: "92.5%", top: "16%", type: "top" },
+ 
+
+    { left: "11.5%", top: "54%", type: "bottom" },
+    { left: "20.5%", top: "54%", type: "bottom" },
+    { left: "28.5%", top: "54%", type: "bottom" },
+    { left: "37.5%", top: "54%", type: "bottom" },
+    { left: "45.5%", top: "54%", type: "bottom" },
+    { left: "54.5%", top: "54%", type: "bottom" },
+    { left: "63.5%", top: "54%", type: "bottom" },
+    { left: "72.5%", top: "54%", type: "bottom" },
+    { left: "81.5%", top: "54%", type: "bottom" }
+  ]
+};
