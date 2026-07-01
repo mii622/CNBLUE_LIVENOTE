9b66d0e72562b307d6915094e989d137c5fe7b39
mii622 <nonnon6219@gmail.com>
2026-07-01 12:44:36 +0900

    Create sticker.js

---
+++ ex/26bk/data/sticker.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  image: "../img/26bksticker.jpg",
+
+  cardWidth: "20%",
+  cardHeight: "60%",
+  markTop: "-8px",
+  markRight: "8px",
+
+  cards: [
+    { left: "2%", top: "16%", type: "top" },
+    { left: "20%", top: "32%", type: "top" },
+    { left: "38%", top: "16%", type: "top" },
+    { left: "60%", top: "32%", type: "top" },
+    { left: "80%", top: "16%", type: "top" },
+  ]
+};
