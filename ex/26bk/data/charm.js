4d7459eb4fe6ee1803a4cbd2482edd3e488c6e50
mii622 <nonnon6219@gmail.com>
2026-07-01 12:44:16 +0900

    Create charm.js

---
+++ ex/26bk/data/charm.js
@@ -0,0 +1,28 @@
+const CONFIG = {
+  image: "../img/26bkcharm.jpg",
+
+  cards: [
+    { left: "28.5%", top: "16%", type: "top" },
+    { left: "37.5%", top: "16%", type: "top" },
+    { left: "50.5%", top: "16%", type: "top" },
+    { left: "63.5%", top: "16%", type: "top" },
+    { left: "73.5%", top: "16%", type: "top" },
+    { left: "88.5%", top: "16%", type: "top" },
+
+    { left: "3.5%", top: "45%", type: "top" },
+    { left: "14.5%", top: "50%", type: "top" },
+    { left: "27.5%", top: "50%", type: "top" },
+    { left: "37.5%", top: "50%", type: "top" },
+    { left: "50.5%", top: "50%", type: "top" },
+    { left: "63.5%", top: "45%", type: "top" },
+    { left: "73.5%", top: "50%", type: "top" },
+    { left: "85.5%", top: "50%", type: "top" },
+    
+    { left: "15.5%", top: "75%", type: "top" },
+    { left: "25.5%", top: "80%", type: "top" },
+    { left: "37.5%", top: "75%", type: "top" },
+    { left: "50.5%", top: "75%", type: "top" },
+    { left: "62.5%", top: "75%", type: "top" },
+    { left: "73.5%", top: "75%", type: "top" }
+  ]
+};
