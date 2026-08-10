2d5361b67f56af1c5cecdfbaf9ac6c34b725d459
mii622 <nonnon6219@gmail.com>
2026-08-10 22:21:22 +0900

    Update badge.js

--- ex/3LOGYTOKYO/data/badge.js
+++ ex/3LOGYTOKYO/data/badge.js
@@ -2,9 +2,9 @@ const CONFIG = {
   image: "../img/badge.jpg",
 
   cardWidth: "20%",
-  cardHeight: "60%",
+  cardHeight: "40%",
   markTop: "-8px",
-  markRight: "8px",
+  markRight: "4px",
   
   cards: [
     { left: "12.5%", top: "15%", type: "top" },
