a75dc636984b473ce9c61e2cd3bd591fda2480fb
mii622 <nonnon6219@gmail.com>
2026-08-10 22:08:30 +0900

    Update trcold.js

--- ex/3LOGYTOKYO/data/trcold.js
+++ ex/3LOGYTOKYO/data/trcold.js
@@ -2,15 +2,14 @@ const CONFIG = {
   image: "../img/trcold.jpg",
 
   cards: [
-    { left: "3.5%", top: "16%", type: "top" },
-    { left: "14.5%", top: "16%", type: "top" },
-    { left: "50.5%", top: "16%", type: "top" },
+    { left: "11.5%", top: "16%", type: "top" },
+    { left: "25.5%", top: "16%", type: "top" },
+    { left: "50%", top: "16%", type: "top" },
     { left: "63.5%", top: "16%", type: "top" },
-    { left: "88%", top: "16%", type: "top" },
-    { left: "15.5%", top: "85%", type: "top" },
-    { left: "37.5%", top: "85%", type: "top" },
-    { left: "50.5%", top: "85%", type: "top" },
-    { left: "62.5%", top: "85%", type: "top" },
-    { left: "73.5%", top: "85%", type: "top" }
+    { left: "87.5%", top: "16%", type: "top" },
+    { left: "15.5%", top: "85%", type: "bottom" },
+    { left: "37.5%", top: "85%", type: "bottom" },
+    { left: "50.5%", top: "85%", type: "bottom" },
+    { left: "73.5%", top: "85%", type: "bottom" }
   ]
 };
