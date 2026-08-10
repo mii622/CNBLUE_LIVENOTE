4010e570b8be556f776909f600169ac52614752a
mii622 <nonnon6219@gmail.com>
2026-08-10 23:54:25 +0900

    Update trcold.js

--- ex/3LOGYTOKYO/data/trcold.js
+++ ex/3LOGYTOKYO/data/trcold.js
@@ -2,7 +2,7 @@ const CONFIG = {
   image: "../img/trcold.jpg",
 
   cardWidth: "15%",
-  cardHeight: "40%"
+  cardHeight: "40%",
 
   cards: [
     { left: "11.5%", top: "16%", type: "top" },
@@ -12,7 +12,7 @@ const CONFIG = {
     { left: "87.5%", top: "16%", type: "top" },
     { left: "15.5%", top: "35%", type: "bottom" },
     { left: "37.5%", top: "35%", type: "bottom" },
-    { left: "50.5%", top: 35%", type: "bottom" },
+    { left: "50.5%", top: "35%", type: "bottom" },
     { left: "73.5%", top: "35%", type: "bottom" }
   ]
 };
