0f31eacdc766d8fe68fe14602fa27c00f74ab304
mii622 <nonnon6219@gmail.com>
2026-08-10 23:52:20 +0900

    Update trc.js

--- ex/3LOGYTOKYO/data/trc.js
+++ ex/3LOGYTOKYO/data/trc.js
@@ -8,13 +8,13 @@ const CONFIG = {
 
   cards: [
     { left: "3.5%", top: "16.5%", type: "top" },
-    { left: "37.5%", top: "16.5%", type: "top" },
-    { left: "58%", top: "16.5%", type: "top" },
+    { left: "20%", top: "16.5%", type: "top" },
+    { left: "48%", top: "16.5%", type: "top" },
     { left: "63.5%", top: "16%", type: "top" },
-    { left: "88.5%", top: "16.5%", type: "top" },
-    { left: "15.5%", top: "75%", type: "top" },
-    { left: "37%", top: "75%", type: "top" },
-    { left: "61%", top: "75%", type: "top" },
-    { left: "74.5%", top: "75%", type: "top" }
+    { left: "88%", top: "16.5%", type: "top" },
+    { left: "15.5%", top: "40%", type: "top" },
+    { left: "37%", top: "40%", type: "top" },
+    { left: "58%", top: "40%", type: "top" },
+    { left: "74.5%", top: "40%", type: "top" }
   ]
 };
